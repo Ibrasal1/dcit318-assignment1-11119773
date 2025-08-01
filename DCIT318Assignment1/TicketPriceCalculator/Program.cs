8fcde22fa0c80787823a44528f73a8b873c1fad7
Ibrasal1 <ibrasal360@gmail.com>
2025-08-01 16:40:40 +0000

    Added Ticket Price Calculator

---
+++ DCIT318Assignment1/TicketPriceCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TICKET PRICE CALCULATOR");
+
+            Console.Write("Enter your age: ");
+            string input = Console.ReadLine();
+            int age;
+
+            if (int.TryParse(input, out age) && age > 0)
+            {
+                decimal price;
+
+                if (age <= 12 || age >= 65)
+                    price = 7.00m;
+                else
+                    price = 10.00m;
+
+                Console.WriteLine($"Your ticket price is: GHC{price}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input! Please enter a valid age.");
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
